71bbfdb2ff635460152428e45d8b23e7c552ada6
telles-dajello <telles.dajello@gmail.com>
2023-09-05 15:27:18 -0300

    changes in ContactsPge. checking duplicates with form and adding contact. passing all to contact form and tileList.

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,34 +3,51 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
-  /*
-  Define state variables for 
-  contact info and duplicate check
-  */
+export const ContactsPage = ({ contacts, addContact }) => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    if (!duplicate) {
+      addContact(name, phone, email)
+      setName('')
+      setPhone('')
+      setEmail('')
+    }
   };
 
-  /*
-  Using hooks, check for contact name in the 
-  contacts array variable in props
-  */
+  useEffect(() => {
+    if (contacts.some((contact) => name === contact.name)) {
+      setDuplicate(true)
+    } else {
+      setDuplicate(false)
+    }
+  },[name, contacts, duplicate])
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact
+          {duplicate ? 'Name already exists': ''}
+          </h2>
+          <ContactForm 
+          name={name}
+          setName={set}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          onSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList items={contacts} />
       </section>
     </div>
   );
-};
+}
+setPhone={setPhone};
