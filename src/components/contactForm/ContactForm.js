0e4d01dac2b31d8298d9b0ae954f12511865662a
telles-dajello <telles.dajello@gmail.com>
2023-09-05 15:56:15 -0300

    changes in ContactForm. took a while to do it in jsx and not the more ingraine pure html way. It was a good review of the attributes for input.

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,45 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input 
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) =>{setName(e.target.value)}}
+            required
+            placeholder="Add name"
+          />
+        </label>
+        <br />
+        <label>
+          <input 
+            type="tel" 
+            name="phone"
+            value={phone}
+            onChange={(e) =>{setPhone(e.target.value)}}
+            required
+            placeholder="Add phone"
+            pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+          />
+        </label>
+        <br />
+        <label>
+          <input 
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) =>{setEmail(e.target.value)}}
+            required
+            placeholder="Add an email"
+          />
+        </label>
+        <br />
+      <input type="submit" value="Add Contact"/>
+      </form>
+    </>
   );
 };
 
